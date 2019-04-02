c5fb95f2e2ed399fcad4e274f47eec1aba607fcf
inhaq <p126013@nu.edu.pk>
2019-04-02 19:56:42 +0500

    module pattern updated

--- js/main.js
+++ js/main.js
@@ -2,106 +2,84 @@
 
 const Board = (() => {
 
-    let inputArr = [];
+    // let userInput = [];
+    let count = 0;
+    let moves = {player1: [], player2: []};
+
     const handleUserInput = () => {
-        let slotsList = document.querySelectorAll('.box');
-        // let slots = Array.from(slotsList);
-        // slotsList.forEach( (slot, i) =>  {
-        //     slot.addEventListener('click',  () => {
-        //         hello(i);
-        //         slotsList[i].textContent = sign;
-        //         inputArr.push(i);
-        //         inputArray();
-        //         console.log(inputArr);
-        //     })
-        // })
-        console.log(slotsList.length);
-        for (let i = 0; i < slotsList.length; i++){
-            let slot = slotsList[i];
-            console.log(i)
-            slot.addEventListener('click', function () {
-                slotsList[i].textContent = 'X';
-                inputArr.push(i);
-                console.log('slot');
-            })
-            // return inputArr;
-        }
+        document.addEventListener('click', function (e) {
+            let userInput = e.target.textContent;
+            userInput = Number(userInput);
+            if (e.target.matches('.box') && !isNaN(userInput)) {
+                if (count % 2 === 0) {
+                    moves.player1.push(userInput - 1);
+                    e.target.textContent = 'X';
+                } else {
+                    moves.player2.push(userInput - 1);
+                    e.target.textContent = 'O';
+                }
+                count++;
+                hasUserInput();
+            } else {
+                alert('DRY');
+            }
+        })
+    };
 
-    }
-
-    const inputArray = () => {
-        return inputArr;
-    }
-    const isWiningMove = () => {
-        const inputArray =  inputArray();
-        //just testing
-        // Basically the algorithm for
-        if (inputArray.length > 3){
-            return true;
-        }
-        return  false;
-    }
+    const hasUserInput = () => {
+        CheckResult.tie(moves);
+        CheckResult.winner(moves);
+        return moves;
+    };
 
     const init = () => {
-        const player1 = Player(prompt(), 'X');
-        const player2 = Player(prompt(), 'O');
-        handleUserInput()
-    }
-
-    return {handleUserInput, init, inputArray};
+        const player1 = Player(prompt());
+        const player2 = Player(prompt());
+        handleUserInput();
+    };
+    return {handleUserInput, init, hasUserInput};
 })();
 
 // players - factory
 
-const Player = (name, sign) => {
+const Player = (name) => {
     const playerName = () => name;
-    const playerSign = () => sign;
-
-    const start = (p1) => {
-
-    }
-
-    return {playerName, playerSign, start}
-}
-
-// console.log(player1.playerSign());
-// start / restart
+    return {playerName}
+};
 
 // game functionality - module
 
-// const CheckResult = function(){
-//     const winningComb = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
-//                         [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-//
-//
-//     const winner = (a) => {
-//         for(let i = 0;i < winningComb.length;i++){
-//             if(winningComb[i] !== a[i]){
-//                 return false;
-//             }
-//         }
-//         console.log('We have a winner');
-//         return true;
-//     }
-//     const checkSigns = (x,y) => {
-//         return x.sign !== y.sign;
-//     }
-//     const bothPlayers = (a,b) => {
-//         return checkSigns(a,b)
-//     };
-//     const legalMove = (a,b) => {
-//         return a.indexOf(b) === -1;
-//     }
-//     const gameOver = (a,b) => {
-//         return winner(a) || b === 9
-//     }
-//
-//     return {
-//         bothPlayers, legalMove, gameOver,winner
-//     }
-// };
+const CheckResult = (() => {
+    const winningComb = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+                        [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
-//
 
-Board.init()
-Board.inputArray();
+    const winner = (moves) => {
+            if (moves.player1.length > 2){
+        for (let i = 0; i < winningComb.length; i++) {
+                if (winningComb[i].indexOf(moves.player1)) {
+                    alert('Winnnnnnnn')
+                }
+            }
+
+        }
+        console.log('We have a winner');
+        return true;
+    };
+
+    const tie = (moves) => {
+        console.log(moves);
+        if (moves.player1.length + moves.player2.length === 9){
+            alert('its tie');
+        }
+    }
+
+    const gameOver = (a, b) => {
+        return winner(a) || b === 9
+    };
+
+    return {gameOver, winner, tie};
+})();
+
+Board.init();
+// Board.inputArray();
