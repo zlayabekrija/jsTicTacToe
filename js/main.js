8fecf81fdd33b062c41520be5b31fb9614425fc8
zlayabekrija <zlaja.bekrija@gmail.com>
2019-04-02 20:44:49 +0200

    'counter_added'

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 // board - module
-
+let firstPlayerScore = document.getElementById('fplayer')
+let secondPlayerScore = document.getElementById('splayer');
 const Board = (() => {
 
     // let userInput = [];
+    let firstPlayer = '';
+    let secondPlayer = '';
+    playOn = true;
     let count = 0;
-    let moves = {player1: [], player2: []};
+    let moves = {
+        player1: [],
+        player2: []
+    };
 
     const handleUserInput = () => {
         document.addEventListener('click', function (e) {
+            e.stopImmediatePropagation();
             let userInput = e.target.textContent;
             userInput = Number(userInput);
-            if (e.target.matches('.box') && !isNaN(userInput)) {
+            if (e.target.matches('.box') && !isNaN(userInput) && playOn) {
                 if (count % 2 === 0) {
                     moves.player1.push(userInput - 1);
                     e.target.textContent = 'X';
@@ -19,67 +27,140 @@ const Board = (() => {
                     e.target.textContent = 'O';
                 }
                 count++;
-                hasUserInput();
-            } else {
+                hasUserInput(firstPlayer, secondPlayer);
+            } else if (e.target.matches('.box') && playOn) {
                 alert('DRY');
             }
         })
     };
 
-    const hasUserInput = () => {
-        CheckResult.tie(moves);
-        CheckResult.winner(moves);
-        return moves;
+    const hasUserInput = (a, b) => {
+        if (CheckResult.gameOver(moves, a, b)) {
+            winCount(CheckResult.count)
+            playOn = false;
+            restartDisplay();
+            innerRestart();
+            return;
+        }
     };
+    const innerRestart = () => {
+        playOn = true;
+        moves = {player1:[],player2: []};
+        count = 0;
+        
+    };
+
+    
 
     const init = () => {
         const player1 = Player(prompt());
         const player2 = Player(prompt());
+        firstPlayer = player1.playerName();
+        secondPlayer = player2.playerName();
+        CheckResult.count.player1 = 0;
+        CheckResult.count.player2 = 0;
+        winCount(CheckResult.count);
+        renderNames(firstPlayer,secondPlayer);
         handleUserInput();
     };
-    return {handleUserInput, init, hasUserInput};
+    return {
+        handleUserInput,
+        init,
+        hasUserInput
+    };
 })();
 
 // players - factory
 
 const Player = (name) => {
     const playerName = () => name;
-    return {playerName}
+    return {
+        playerName
+    }
 };
 
 // game functionality - module
 
 const CheckResult = (() => {
-    const winningComb = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
-                        [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    const winningComb = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    let count = {player1: 0, player2: 0}
 
-
-    const winner = (moves) => {
-            if (moves.player1.length > 2){
-        for (let i = 0; i < winningComb.length; i++) {
-                if (winningComb[i].indexOf(moves.player1)) {
-                    alert('Winnnnnnnn')
+    const winner = (moves, firstPlayer, secondPlayer) => {
+        if (moves.player1.length > 2) {
+            for (let i = 0; i < winningComb.length; i++) {
+                if (moves.player1.indexOf(winningComb[i][0]) !== -1 && moves.player1.indexOf(winningComb[i][1]) !== -1 && moves.player1.indexOf(winningComb[i][2]) !== -1) {
+                    alert(firstPlayer + " wins");
+                    count.player1++;
+                    return true
                 }
+                if (moves.player2.indexOf(winningComb[i][0]) !== -1 && moves.player2.indexOf(winningComb[i][1]) !== -1 && moves.player2.indexOf(winningComb[i][2]) !== -1) {
+                    alert(secondPlayer + " wins");
+                    count.player2++;
+                    return true;
+                }
+
             }
 
         }
-        console.log('We have a winner');
-        return true;
+        return false;
     };
 
     const tie = (moves) => {
-        console.log(moves);
-        if (moves.player1.length + moves.player2.length === 9){
-            alert('its tie');
+        let num = 0;
+        for (let count in moves) {
+            num += moves[count].length;
         }
+        if (num === 9) {
+            return true;
+        }
+        return false;
     }
 
-    const gameOver = (a, b) => {
-        return winner(a) || b === 9
+    const gameOver = (moves, firstPlayer, secondPlayer) => {
+        return winner(moves, firstPlayer, secondPlayer) || tie(moves) 
     };
 
-    return {gameOver, winner, tie};
+    return {
+        gameOver,
+        winner,
+        tie,
+        count
+    };
 })();
 
-Board.init();
-// Board.inputArray();
+// Board.init();
+let start = document.querySelector('.start');
+start.addEventListener('click',()=>{
+    restartDisplay();
+    setTimeout(() => {
+        Board.init();
+    }, 0);
+    
+});
+
+function restartDisplay (){
+    let boxes = document.querySelectorAll('.box');
+    for(let i = 0; i < boxes.length; i++){
+        boxes[i].innerHTML = (i + 1).toString();
+    }
+      
+
+}
+function renderNames(firstPlayer,secondPlayer){
+    document.querySelector('.firstplayer').innerHTML = firstPlayer;
+    document.querySelector('.secondplayer').innerHTML = secondPlayer;
+}
+const winCount = (totalscore) => {
+  
+    firstPlayerScore.textContent = totalscore.player1;
+    secondPlayerScore.textContent = totalscore.player2;
+}
